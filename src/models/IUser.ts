5adebb32e2fc1cce14ac3f551fd2521295ccad2f
voikin <woysen@gmail.com>
2023-05-28 03:32:54 +0300

    сверстал страничку профиля пользователя

--- src/models/IUser.ts
+++ src/models/IUser.ts
@@ -1,5 +1,13 @@
+import { IRation } from './IRation';
 export interface IUser {
 	email?: string
 	isActivated?: boolean
 	id?: string
+}
+export interface UserInfo {
+	age: number
+	height: number
+	weight: number
+	preferences: string[]
+	triedRations: IRation[]
 }
